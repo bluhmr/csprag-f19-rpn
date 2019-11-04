6bdbd54525e1a22c7c99e286ad12f4f541abd2fe
bluhmr <bluhmr@umich.edu>
2019-11-04 14:40:15 -0500

    added termcolor to the project, modified output

--- rpn.py
+++ rpn.py
@@ -2,6 +2,10 @@
 
 import operator
 
+import sys
+
+from termcolor import colored
+
 
 operators = {
     '+': operator.add,
@@ -22,6 +26,18 @@ def calculate(myarg):
     for token in myarg.split():
         try:
             token = int(token)
+            if (token > 0):
+                print(colored(token, 'green'), end = ' ')
+            elif (token < 0):
+                print(colored(token, 'red', attrs=['bold']), end = ' ')
+            else:
+                print(colored(token, 'blue'), end = ' ')
+        except ValueError:
+            print(colored(token, 'magenta', attrs=['blink', 'bold', 'dark']))
+            
+    for token in myarg.split():
+        try:
+            token = int(token)
             stack.append(token)
         except ValueError:
             function = operators[token]
@@ -29,7 +45,7 @@ def calculate(myarg):
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-        print(stack)
+        #print(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
@@ -37,7 +53,13 @@ def calculate(myarg):
 def main():
     while True:
         result = calculate(input("rpn calc> "))
-        print("Result: ", result)
+        if (result < 0):
+            print(colored(result, 'red', attrs=['bold']))
+        elif (result > 0):
+            print(colored(result, 'green'))
+        else:
+            print(colored(result, 'blue'))
+
 
 if __name__ == '__main__':
     main()
