c2bd5c51bc2c59d9d643b4f667bf48a116472de7
bluhmr <bluhmr@umich.edu>
2019-11-04 18:09:22 -0500

    added a bunch to test coverage

--- rpn.py
+++ rpn.py
@@ -14,8 +14,8 @@ operators = {
     '/': operator.truediv,
     '^': operator.pow,
     '%': operator.mod,
-    '>>': operator.lshift
-    '<<': operator.rshift
+    '>>': operator.lshift,
+    '<<': operator.rshift,
     '|': operator.or_,
     '&': operator.and_,
 
